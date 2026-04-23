ebecd35049203631f7e20cdaef1d04315f58df52
saurabh0jha <saurabh.ojha@getweave.com>
2026-04-23 12:27:37 +0530

    feat: initializing a server with a serve mux and address but no handlers

---
+++ go.mod
@@ -0,0 +1,3 @@
+module saurabh/chirpy.com/m
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	httpServer := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	httpServer.ListenAndServe()
+}
