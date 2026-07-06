c828d6316286eb97f6ac3f4ea61fff0c3c9c589a
Saurabh0Two1 <ojha.saurabh10@gmail.com>
2026-07-06 19:57:10 +0530

    feat: sorting

--- handlerGetAllChirps.go
+++ handlerGetAllChirps.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"slices"
 	"time"
 
 	"saurabh/chirpy.com/m/internal/database"
@@ -33,16 +34,36 @@ func (cfg *apiConfig) GetChirps(w http.ResponseWriter, r *http.Request, authorUu
 
 }
 
+func authorIDFromRequest(r *http.Request) (uuid.UUID, error) {
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString == "" {
+		return uuid.Nil, nil
+	}
+	authorID, err := uuid.Parse(authorIDString)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return authorID, nil
+}
+
 func (cfg *apiConfig) GetAllChirpsHandler(w http.ResponseWriter, r *http.Request) {
-
-	authorId := r.URL.Query().Get("author_id")
-
-	authorUuid, err := uuid.Parse(authorId)
+	authorID, err := authorIDFromRequest(r)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "author ID is invalid", err)
+		return
 	}
 
-	chirps, err := cfg.GetChirps(w, r, authorUuid)
+	chirps, err := cfg.GetChirps(w, r, authorID)
+
+	sortDirection := r.URL.Query().Get("sort")
+
+	slices.SortStableFunc(chirps, func(a, b database.Chirp) int {
+		if sortDirection == "desc" {
+			return b.CreatedAt.Compare(a.CreatedAt)
+		}
+
+		return a.CreatedAt.Compare(b.CreatedAt)
+	})
 
 	type successResp struct {
 		ID        string `json:"id"`
